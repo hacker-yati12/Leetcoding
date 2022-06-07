228caca450a4f15213be6d95a4de49b3fc8db50f
hacker-yati12 <101984753+hacker-yati12@users.noreply.github.com>
2022-06-08 02:37:50 +0530

    Time: 93 ms (52.08%), Space: 48.4 MB (44.08%) - LeetHub

--- 55-jump-game/55-jump-game.cpp
+++ 55-jump-game/55-jump-game.cpp
@@ -1,18 +1,13 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        int n=nums.size();
-        int last=n-1;
-        for(int i=n-2;i>=0;i--)
-        {
-            if(i+nums[i]>=last)
-            {  
-                last=i;
-            }
+        int n = nums.size();
+        int jump = nums[0], i = 0;
+        while (i <= jump && i < n) {
+            jump = max(jump, i + nums[i]);
+            i++;
         }
-        if(last<=0)
-            return true;
-        else
-            return false;
+        if (jump >= n-1) return true;
+        return false;
     }
 };
