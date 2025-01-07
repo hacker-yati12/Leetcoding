9c0c23a22a7739697ec3f8de0c8d03bfc64acf28
Yatindra Deo <101984753+hacker-yati12@users.noreply.github.com>
2025-01-07 06:31:35 +0530

    Time: 0 ms (100%), Space: 11.2 MB (76.01%) - LeetHub

--- 1408-string-matching-in-an-array/1408-string-matching-in-an-array.cpp
+++ 1408-string-matching-in-an-array/1408-string-matching-in-an-array.cpp
@@ -1,29 +1,19 @@
 class Solution {
 public:
     vector<string> stringMatching(vector<string>& words) {
-        int n = words.size();
-        vector<string> ans;
-        for(int i = 0; i < n; i++) {
-            int szi = words[i].size();
-            for(int j = 0; j < n; j++) {
-                if(j == i)
-                    continue;
-                int szj = words[j].size();
-                if(szi > szj)
-                    continue;
-                bool flag = false;
-                for(int k = 0; k <= szj-szi; k++) {
-                    string str = words[j].substr(k, szi);
-                    if(str == words[i]) {
-                        ans.push_back(words[i]);
-                        flag = true;
-                        break;
-                    }
-                }
-                if(flag)
-                    break;
+        vector<string> str;
+
+        for (int i = 0; i < words.size(); i++) {
+            for (int j = 0; j < words.size(); j++) {
+               if (i != j) {
+                   auto found = words[j].find(words[i]);
+                   if (found != string::npos) {
+                       str.push_back(words[i]);
+                       break;
+                   }
+               }
             }
         }
-        return ans;
+        return str;
     }
 };
