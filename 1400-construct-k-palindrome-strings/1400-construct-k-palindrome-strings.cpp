064e4cd38b8838a2084a43b2c36e58e1b2d91917
Yatindra Deo <101984753+hacker-yati12@users.noreply.github.com>
2025-01-11 13:26:16 +0530

    Time: 3 ms (72.03%), Space: 14.8 MB (78.67%) - LeetHub

--- 1400-construct-k-palindrome-strings/1400-construct-k-palindrome-strings.cpp
+++ 1400-construct-k-palindrome-strings/1400-construct-k-palindrome-strings.cpp
@@ -4,17 +4,14 @@ public:
         if(s.size() < k)
             return false;
         vector<int> freq(26, 0);
-        for(auto x: s) {
+        for(auto &x: s) {
             freq[x-'a']++;
         }
         int count = 0;
         for(int i=0; i<26; i++) {
-            if(freq[i]%2 != 0)
+            if(freq[i]&1)
                 count++;
         }
-        if(count<=k)
-            return true;
-        else
-            return false;
+        return count<=k;
     }
 };
