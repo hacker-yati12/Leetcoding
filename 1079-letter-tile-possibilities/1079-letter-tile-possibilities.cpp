8c80c9c8959aa0d65c8982a45185f3d95a40744c
Yatindra Deo <101984753+hacker-yati12@users.noreply.github.com>
2025-02-18 01:03:52 +0530

    Time: 0 ms (100%), Space: 7.9 MB (84.16%) - LeetHub

--- 1079-letter-tile-possibilities/1079-letter-tile-possibilities.cpp
+++ 1079-letter-tile-possibilities/1079-letter-tile-possibilities.cpp
@@ -1,25 +1,21 @@
 class Solution {
-    void solve(string tiles, unordered_set<string>& st, vector<bool>& used, string curr, int idx) {
-        if(idx == tiles.size())
-            return;
-        for(int i=0; i<tiles.size(); i++) {
-            if(used[i])
-                continue;
-            used[i] = true;
-            curr += tiles[i];
-            st.insert(curr);
-            solve(tiles, st, used, curr, idx+1);
-            used[i] = false;
-            curr.pop_back();
-            // solve(tiles, st, used, curr, idx+1);
+    void solve(unordered_map<char, int>& mp, int &ans) {
+        for(auto &x: mp) {
+            if(x.second > 0) {
+                x.second--;
+                solve(mp, ++ans);
+                x.second++;
+            }                
         }
         return;
     }
 public:
     int numTilePossibilities(string tiles) {
-        unordered_set<string> st;
-        vector<bool> used(tiles.size(), false);
-        solve(tiles, st, used, "", 0);
-        return st.size();
+        unordered_map<char, int> mp;
+        for(char &x: tiles)
+            mp[x]++;
+        int ans = 0;
+        solve(mp, ans);
+        return ans;
     }
 };
