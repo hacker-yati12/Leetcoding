ac06febdd8cdf7bf360b7db5ca66ba8732857f30
Yatindra Deo <101984753+hacker-yati12@users.noreply.github.com>
2025-03-03 12:23:40 +0530

    Time: 99 ms (97.15%), Space: 147.7 MB (38.85%) - LeetHub

--- 0873-length-of-longest-fibonacci-subsequence/0873-length-of-longest-fibonacci-subsequence.cpp
+++ 0873-length-of-longest-fibonacci-subsequence/0873-length-of-longest-fibonacci-subsequence.cpp
@@ -2,35 +2,22 @@ class Solution {
 public:
     int lenLongestFibSubseq(vector<int>& arr) {
         int n = arr.size();
-        // dp[prev][curr] stores length of Fibonacci sequence ending at indexes
-        // prev,curr
-        vector<vector<int>> dp(n, vector<int>(n, 0));
-        int maxLen = 0;
-
-        // Find all possible pairs that sum to arr[curr]
-        for (int curr = 2; curr < n; curr++) {
-            // Use two pointers to find pairs that sum to arr[curr]
-            int start = 0;
-            int end = curr - 1;
-
-            while (start < end) {
-                int pairSum = arr[start] + arr[end];
-
-                if (pairSum > arr[curr]) {
-                    end--;
-                } else if (pairSum < arr[curr]) {
-                    start++;
-                } else {
-                    // Found a valid pair, update dp
-                    dp[end][curr] = dp[start][end] + 1;
-                    maxLen = max(dp[end][curr], maxLen);
-                    end--;
-                    start++;
+        vector<vector<int>> dp(n, vector<int> (n, 0));
+        int ans = 0;
+        for(int k=2; k<n; k++) {
+            int i=0, j=k-1;
+            while(i<j) {
+                if(arr[i]+arr[j]<arr[k])
+                    i++;
+                else if(arr[i]+arr[j]>arr[k])
+                    j--;
+                else {
+                    dp[j][k] = dp[i][j] + 1;
+                    ans = max(ans, dp[j][k]);
+                    i++, j--;
                 }
             }
         }
-
-        // Add 2 to include first two numbers, or return 0 if no sequence found
-        return maxLen == 0 ? 0 : maxLen + 2;
+        return (ans==0)?0:ans+2;
     }
 };
