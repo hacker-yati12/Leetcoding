194f794ff851d984600b3e20c56abf9d4dbdec9e
Yatindra Deo <101984753+hacker-yati12@users.noreply.github.com>
2025-03-05 18:40:20 +0530

    Time: 0 ms (100%), Space: 7.9 MB (31.17%) - LeetHub

--- 2579-count-total-number-of-colored-cells/2579-count-total-number-of-colored-cells.cpp
+++ 2579-count-total-number-of-colored-cells/2579-count-total-number-of-colored-cells.cpp
@@ -1,11 +1,6 @@
 class Solution {
 public:
     long long coloredCells(int n) {
-        long long ans = 1;
-        int ctr = 1;
-        while(--n) {
-            ans += 4*(long long)(ctr++);
-        }
-        return ans;
+        return 1+4*((long long)n*(n-1))/2;
     }
 };
