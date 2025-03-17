23aef27a879fc997bca8d642bf2fabc4d1bc0624
Yatindra Deo <101984753+hacker-yati12@users.noreply.github.com>
2025-03-17 10:33:25 +0530

    Time: 0 ms (100%), Space: 16.7 MB (84.67%) - LeetHub

---
+++ 2206-divide-array-into-equal-pairs/2206-divide-array-into-equal-pairs.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    bool divideArray(vector<int>& nums) {
+        bitset<501>b;
+        for(auto &x: nums) {
+            b.flip(x);
+        }
+        return b.none();
+    }
+};
