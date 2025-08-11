4be59542bff373c046f60136542046903eeda463
Yatindra Deo <101984753+hacker-yati12@users.noreply.github.com>
2025-08-11 10:16:55 +0530

    Time: 39 ms (11.01%), Space: 9 MB (50.15%) - LeetHub

--- 0869-reordered-power-of-2/0869-reordered-power-of-2.cpp
+++ 0869-reordered-power-of-2/0869-reordered-power-of-2.cpp
@@ -2,9 +2,11 @@ class Solution {
 public:
     bool reorderedPowerOf2(int n) {
         string s = to_string(n);
+        sort(s.begin(), s.end());
         do {
+            if(s[0] == '0') continue;
             int n1 = stoi(s);
-            if(log2(n1) == (int)log2(n1*1.0))
+            if((n1 & (n1 - 1)) == 0)
                 return true;
         } while(next_permutation(s.begin(), s.end()));
         return false;
