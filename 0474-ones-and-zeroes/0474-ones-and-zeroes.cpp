a6d931cd4461e9c7f499954b4d5a359401213c78
Yatindra Deo <101984753+hacker-yati12@users.noreply.github.com>
2025-11-11 17:46:52 +0530

    Time: 49 ms (85.98%), Space: 13.7 MB (84.01%) - LeetHub

---
+++ 0474-ones-and-zeroes/0474-ones-and-zeroes.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int findMaxForm(vector<string>& strs, int m, int n) {
+        vector<vector<int>> dp(m + 1, vector<int> (n + 1, 0));
+        for (auto &x: strs) {
+            int c0 = 0, c1 = 0;
+            for (auto &y: x) {
+                if (y == '1') c1++;
+                else c0++;
+            }
+            for (int i = m; i >= c0; i--) {
+                for (int j = n; j >= c1; j--) {
+                    dp[i][j] = max(dp[i][j], 1 + dp[i - c0][j - c1]);
+                }
+            }
+        }
+        return dp[m][n];
+    }
+};
